bcace6f3ee9084f328c385edc4d2c0068724146a
Abhishek Kumar <96008225+killer-abhi@users.noreply.github.com>
2024-01-11 11:21:01 +0530

    Update PersonalData.js

--- src/Data/PersonalData.js
+++ src/Data/PersonalData.js
@@ -7,7 +7,7 @@ const PersonalData = {
     address:"India",
     aboutMe:"Hi Everyone, I am Abhishek Kumar from Uttar Pradesh, India.I learned C, C++, Python, JavaScript etc, Worked with libraries & frameworks like ReactJS, NodeJS. Have a keen knowledge of databases like MySQL, MongoDB and Firebase. Eager to contribute to innovative projects and gain hands-on experience in a dynamic and collaborative",
     resumeLink:'https://drive.google.com/file/d/1vfnzDcN4uATaNZ7cc9kdKvep9ZBUN_be/view?usp=sharing',
-    imageUrl:"https://drive.google.com/uc?export=view&id=1bAAQPj6lDBsbf-MvHYxNGr9m-p5Yc7uA",
+    imageUrl:"https://drive.usercontent.google.com/download?id=1bAAQPj6lDBsbf-MvHYxNGr9m-p5Yc7uA&export=view",
 }
 export const autoTypeData=['3rd Year IT Grad', 'MERN Stack Developer','Flutter App Developer', 'Open Source Contributor'];
 export default PersonalData;
